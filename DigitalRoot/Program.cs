3a55ad6d0ef9a4605c5e725940dd143519b5784e
SeedPaths-User <lessdismissive@email.com>
2014-06-03 16:03:39 -0600

    DigitalRootV1.0

---
+++ DigitalRoot/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitalRoot("54675566");
+
+
+        } 
+   
+
+
+        
+        static void DigitalRoot(string rootThis)
+        {
+            while (rootThis > 1)
+            {
+                rootThis. == rootingFunc;
+            }
+            
+
+            
+          
+        }
+        static void rootingFunc(string root)
+        {
+            var total = 0;
+            for (var i = 0; i < root.Length; i++)
+            {
+                var rootNumber = root[i].ToString();
+                var number = Convert.ToInt32(rootNumber);
+                total = total + number;
+            }
+            Console.WriteLine(total);
+
+            Console.ReadKey();
+        }
+    } 
+        
+   
+
+
+}
